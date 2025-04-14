de5e8c4b20ebc777668d69e27f79d517cca2983c
LuizGarbini <garbasneto@gmail.com>
2025-04-13 22:06:58 -0300

    Criando o Middleware de Tratamento de Exceções

---
+++ src/middleware/error-handling.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from "express";
+
+import { AppError } from "@/utils/AppError";
+
+export function errorHandling(
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	error: any,
+	request: Request,
+	response: Response,
+	_: NextFunction,
+) {
+	if (error instanceof AppError) {
+		return response.status(error.statusCode).json({ message: error.message });
+	}
+
+	return response.status(500).json({ message: error.message });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { errorHandling } from "./middleware/error-handling";
 import { routes } from "./routes";
 
 const PORT = 3333;
@@ -7,5 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(errorHandling);
 
 app.listen(PORT, () => console.log(`Server ir running on port ${PORT}`));
