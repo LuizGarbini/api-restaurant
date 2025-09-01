8a39cb39407e22590e061b624835a98810409cec
LuizGarbini <garbasneto@gmail.com>
2025-09-01 16:43:53 -0300

    Tratando Dados do Pedido.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ordersRoutes } from "./order-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use("/tables-sessions", tablesSessionsRoutes);
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
